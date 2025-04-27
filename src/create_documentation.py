024df27396d851caa4e40b390e706ad10b1684d2
Brent Nelson <brent.nelson@espressive.com>
2025-04-27 12:13:25 -0700

    deduplicate genearting output for items/sets

--- src/create_documentation.py
+++ src/create_documentation.py
@@ -17,91 +17,84 @@ def make_html_footer():
     output += "</body></html>"
     return output
 
-all_set_items_out = {"html": "", "txt": ""}
-all_unique_items_out = {"html": "", "txt": ""}
 all_items_out = {"html": "", "txt": ""}
-all_sets_out = {"html": "", "txt": ""}
 
 os.makedirs("../html", exist_ok=True)
 os.makedirs("../txt", exist_ok=True)
 
-items = documenter.get_set_item_objects()
-for name in items.keys():
-    for output_format in ["html", "txt"]:
-        base_filename = items[name].get_base_filename(output_format=output_format)
-        filename = f"../{output_format}/{base_filename}"
-        out = items[name].get_text(show_hidden=False, output_format=output_format)
-        all_set_items_out[output_format] += out
-        all_items_out[output_format] += out
-        with open(filename, "w") as f:
-            if output_format == "html":
-                file_out = make_html_header(name, items[name].get_nice_name()) + out + make_html_footer()
-            else:
-                file_out = out
-            f.write(file_out)
-            f.close()
-
-items = documenter.get_unique_item_objects()
-for name in items.keys():
-    for output_format in ["html", "txt"]:
-        base_filename = items[name].get_base_filename(output_format=output_format)
-        filename = f"../{output_format}/{base_filename}"
-        out = items[name].get_text(show_hidden=False, output_format=output_format)
-        all_unique_items_out[output_format] += out
-        all_items_out[output_format] += out
-        with open(filename, "w") as f:
-            if output_format == "html":
-                file_out = make_html_header(name, items[name].get_nice_name()) + out + make_html_footer()
-            else:
-                file_out = out
-            f.write(file_out)
-            f.close()
-
-items = documenter.get_set_objects()
-for name in items.keys():
-    for output_format in ["html", "txt"]:
-        base_filename = items[name].get_base_filename(output_format=output_format)
-        filename = f"../{output_format}/{base_filename}"
-        out = items[name].get_text(show_hidden=False, output_format=output_format)
-        all_sets_out[output_format] += out
-        with open(filename, "w") as f:
-            if output_format == "html":
-                file_out = make_html_header(name, items[name].get_nice_name()) + out + make_html_footer()
-            else:
-                file_out = out
-            f.write(file_out)
-            f.close()
+def generate_individual_output_files(items):
+    items_out = {"html": "", "txt": ""}
+    items = documenter.get_set_item_objects()
+    for name in items.keys():
+        for output_format in ["html", "txt"]:
+            base_filename = items[name].get_base_filename(output_format=output_format)
+            filename = f"../{output_format}/{base_filename}"
+            out = items[name].get_text(show_hidden=False, output_format=output_format)
+            items_out[output_format] += out
+            with open(filename, "w") as f:
+                if output_format == "html":
+                    nn = items[name].get_nice_name()
+                    file_out = make_html_header(name, nn) + out + make_html_footer()
+                else:
+                    file_out = out
+                f.write(file_out)
+                f.close()
+    return items_out
 
 
-
-with open("../html/all_unique_items.html", "w") as f:
-    f.write(make_html_header("BTDiablo Uniques", "BTDiablo_Uniques") + all_unique_items_out["html"] + make_html_footer())
-    f.close()
-
+set_items_out = generate_individual_output_files(documenter.get_set_item_objects())
 with open("../html/all_set_items.html", "w") as f:
-    f.write(make_html_header("BTDiablo Set Items", "BTDiablo_Set_Items") + all_set_items_out["html"] + make_html_footer())
+    f.write(
+            make_html_header("BTDiablo Set Items", "BTDiablo_Set_Items") + 
+            set_items_out["html"] + 
+            make_html_footer())
     f.close()
+with open("../txt/all_set_items.txt", "w") as f:
+    f.write(set_items_out["txt"])
+    f.close()
+all_items_out["html"] += set_items_out["html"]
+all_items_out["txt"] += set_items_out["txt"]
+
+
+
+unique_items_out = generate_individual_output_files(documenter.get_unique_item_objects())
+with open("../html/all_unique_items.html", "w") as f:
+    f.write(
+            make_html_header("BTDiablo Unique Items", "BTDiablo_Unique_Items") + 
+            unique_items_out["html"] + 
+            make_html_footer())
+    f.close()
+with open("../txt/all_unique_items.txt", "w") as f:
+    f.write(unique_items_out["txt"])
+    f.close()
+all_items_out["html"] += unique_items_out["html"]
+all_items_out["txt"] += unique_items_out["txt"]
+
+
+sets_out = generate_individual_output_files(documenter.get_set_objects())
+with open("../html/all_sets.html", "w") as f:
+    f.write(
+            make_html_header("BTDiablo Unique Items", "BTDiablo_Unique_Items") + 
+            sets_out["html"] + 
+            make_html_footer())
+    f.close()
+with open("../txt/all_sets.txt", "w") as f:
+    f.write(sets_out["txt"])
+    f.close()
+
+
+
+
+
+
+
+
 
 with open("../html/all_items.html", "w") as f:
     f.write(make_html_header("BTDiablo Items", "BTDiablo_Items") + all_items_out["html"] + make_html_footer())
-    f.close()
-
-with open("../txt/all_unique_items.txt", "w") as f:
-    f.write(all_unique_items_out["txt"])
-    f.close()
-
-with open("../txt/all_set_items.txt", "w") as f:
-    f.write(all_set_items_out["txt"])
     f.close()
 
 with open("../txt/all_items.txt", "w") as f:
     f.write(all_items_out["txt"])
     f.close()
 
-with open("../html/all_sets.html", "w") as f:
-    f.write(make_html_header("BTDiablo Sets", "Sets_Items") + all_sets_out["html"] + make_html_footer())
-    f.close()
-
-with open("../txt/all_sets.text", "w") as f:
-    f.write(all_sets_out["txt"])
-    f.close()
