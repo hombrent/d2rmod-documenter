cb0e0b1064f34ec73f904ce88836369f599ec12a
Brent Nelson <brent.nelson@espressive.com>
2025-05-18 14:32:49 -0700

    upload articles from files in docs dir

--- src/btd_wiki_login.py
+++ src/btd_wiki_login.py
@@ -2,12 +2,17 @@
 
 import requests
 import os
+import sys
+import ssl
 from getpass import getpass
 
 class BTDWikiLogin:
     def __init__(self):
         self.api_url = "https://btd.miraheze.org/w/api.php"
+        #context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        #context.minimum_version = ssl.TLSVersion.TLSv1_3
         self.session = requests.Session()
+        #self.session.ssl_context = context
         self.session.headers = {
             'User-Agent': 'Mozilla/5.0'
         }
@@ -39,7 +44,7 @@ class BTDWikiLogin:
             return "Success" in str(r.json())
             
         except Exception as e:
-            print(f"Error: {str(e)}")
+            print(f"Error: {str(e)} using: {username} // {password}")
             return False
 
     def upsert_page(self, title, content):
@@ -77,17 +82,49 @@ class BTDWikiLogin:
             print(f"Error editing page: {str(e)}")
             return False
 
+def list_files_recursive(folder_path):
+    """
+    Recursively lists all files in a directory and its subdirectories.
+
+    Args:
+        folder_path (str): The path to the directory.
+
+    Returns:
+        list: A list of file paths.
+    """
+    file_paths = []
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            file_paths.append(file_path)
+    return file_paths
+
 def main():
-    username = "foo"
-    password = "bar"
-    
+    from btd_wiki_creds import btd_creds
+    #print(f"username: {btd_creds['username']} password: {btd_creds['password']}")
+    #print(f"openssl version {ssl.OPENSSL_VERSION}")
+
     wiki = BTDWikiLogin()
-    if wiki.login(username, password):
+    if wiki.login(btd_creds["username"], btd_creds["password"]):
         print("Login successful")
-        # Update dev page
-        wiki.upsert_page("Dev", "dev page. all under construction")
     else:
         print("Login failed")
+        sys.exit()
+
+    basedir = "../docs"
+    wikifiles = list_files_recursive(basedir)
+    #wikifiles = ["../docs/Items/Unique/Ravaging_T1"]
+    for wikifile in wikifiles:
+        wikipath = wikifile.lstrip(basedir)
+        print(f"{wikifile} {wikipath}")
+        with open(wikifile, "r") as file:
+            contents = file.read()
+            print("==================")
+            print(contents)
+            print("\n\n\n\n")
+            wiki.upsert_page(wikipath, contents)
+
+    
 
 if __name__ == "__main__":
     main() 
